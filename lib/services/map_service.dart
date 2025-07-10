3d70b686d53ed2a304d518d06a82bd94a4bdc177
kawi15 <karol.m.wisniewski15@gmail.com>
2025-07-10 16:06:44 +0200

    Add documentation for MapService functions

--- lib/services/map_service.dart
+++ lib/services/map_service.dart
@@ -6,12 +6,14 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapService {
   //TODO przerzucić tu funkcje z home screen
 
+  /// Function that decodes Polylines to List of [LatLng]
   static List<LatLng> decodePolyline(String encoded) {
     final polylinePoints = PolylinePoints();
     final points = polylinePoints.decodePolyline(encoded);
     return points.map((p) => LatLng(p.latitude, p.longitude)).toList();
   }
 
+  /// Function that checks if [points] are visible on given [bounds]
   static bool polylineIntersectsBounds(List<LatLng> points, LatLngBounds bounds) {
     return points.any((point) =>
     point.latitude >= bounds.southwest.latitude &&
